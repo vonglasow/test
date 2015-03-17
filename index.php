d05383e3eb322438193cecbec0da90a3ba6a039e
Alexis von Glasow <alexis.von-glasow@inovia-team.com>
2015-03-17 16:09:57 +0100

    Add foo class to tests rules with method name should begin with is and has

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Class foo test
+ *
+ * @category Test
+ * @package  Foo
+ */
+class foo
+{
+    /**
+     * Check if ok
+     *
+     * @return boolean true
+     */
+    public function isOk()
+    {
+        return true;
+    }
+
+    /**
+     * Check if has something
+     *
+     * @return boolean true
+     */
+    public function hasSomething()
+    {
+        return true;
+    }
+
+    /**
+     * Check if does things
+     *
+     * @return boolean true
+     */
+    public function doesThings()
+    {
+        return true;
+    }
+}
