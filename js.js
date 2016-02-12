22c8be54118831b1dcefc97da11dba49816d5964
Alexis von Glasow <alexis.von-glasow@inovia-team.com>
2016-02-12 16:14:12 +0100

    add js file

---
+++ js.js
@@ -0,0 +1,21 @@
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is licensed exclusively to Inovia Team.
+ *
+ * @copyright   Copyright (c) 2016 Inovia Team (http://www.inovia.fr)
+ * @license     All rights reserved
+ * @author      The Inovia Dev Team
+ */
+(function () {
+    'use strict';
+
+    angular.module('MobileApps')
+        .run(['LegacyProjectService', 'NavBarNavigationService', function (LegacyProjectService, NavBarNavigationService) {
+            LegacyProjectService.mobileApps = {
+                setMenuUrl: function (url) {
+                    NavBarNavigationService.setMenuUrl(url);
+                }
+            };
+        }]);
+})();
