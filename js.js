ab085a20331ed38d90fc68b6416105828c6ceb41
Alexis von Glasow <alexis.von-glasow@inovia-team.com>
2016-02-12 16:22:29 +0100

    add header format in file

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is licensed exclusively to Inovia Team.
+ *
+ * @copyright   Copyright (c) 2016 Inovia Team (http://www.inovia.fr)
+ * @license     All rights reserved
+ * @author      The Inovia Dev Team
+ */
+
 /*
  * SonarQube, open source software quality management tool.
  * Copyright (C) 2008-2013 SonarSource
