4038f4c55d06f950b22e26c1e2314800be7496f3
Alexis von Glasow <ashgenesis@gmail.com>
2017-01-09 21:38:17 +0100

    Quality: Happy new year!

--- Report/Cli/Colorizer.php
+++ Report/Cli/Colorizer.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -33,11 +33,10 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
 namespace Hoa\Test\Report\Cli;
 
-use Hoa\Console;
 use atoum;
+use Hoa\Console;
 
 class Colorizer extends atoum\cli\colorizer
 {
--- Report/Cli/Fields/Result.php
+++ Report/Cli/Fields/Result.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2016, Hoa community. All rights reserved.
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -33,7 +33,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
 namespace Hoa\Test\Report\Cli\Fields;
 
 use atoum\report\fields;
